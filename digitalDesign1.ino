0dea26be61245895302e56aa99847859071295cd
NikosMouzakitis <mzktsn@gmail.com>
2016-03-21 02:50:49 +0200

    Update digitalDesign1.ino

--- digitalDesign1.ino
+++ digitalDesign1.ino
@@ -4,7 +4,7 @@
 //CIRCUIT/SCHEMMATIC: https://123d.circuits.io/circuits/1808608-implementation-digital-gates-boolean-algebra-digital-design
 //-------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------This circuit implements the boolean algebra,------------------------------------------ 
-//-----------------------------------in the function G =[ (x*y) + (x+y) ] for x,y boolean values.
+//-----------------------------------in the function G =[ (x*y) + (x+y) ] for x,y boolean values,using an Arduino board.---
 //----------------------------------------------digitalDesign1.ino-----------------------------------March 2016------------
 
 void setup()
@@ -19,30 +19,30 @@ void loop()
   	v1 = 0;
   	v2 = 1;
 
-	  digitalWrite(7,v1);
+        digitalWrite(7,v1);
   	digitalWrite(6,v2);
   	i = digitalRead(5);
   	Serial.print("x=");
-	  Serial.print(v1);
+        Serial.print(v1);
   	Serial.print(" y=");
   	Serial.println(v2);
   	Serial.print("Function Output:");
   	Serial.print(i);
-    Serial.println();
+        Serial.println();
   	Serial.println();
   	delay(d_time);
   
-	  digitalWrite(7,v2);
+        digitalWrite(7,v2);
   	digitalWrite(6,v2);
   	i = digitalRead(5);
-  	Serial.print("x=");
-	  Serial.print(v2);
+  	Serial.print("x=");	
+  	Serial.print(v2);
   	Serial.print(" y=");
   	Serial.println(v2);
   	Serial.print("Function Output:");
   	Serial.print(i);
-    Serial.println();
-    Serial.println();               
+    	Serial.println();
+        Serial.println();               
   	delay(d_time);
   
   
@@ -55,7 +55,7 @@ void loop()
   	Serial.println(v1);
   	Serial.print("Function Output:");
   	Serial.print(i);
-    Serial.println();
+        Serial.println();
   	Serial.println();
   	delay(d_time);
 }
